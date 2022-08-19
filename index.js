85628b8ef28418ef379ba44c97039aa1fdc6e0f7
Willzi3 <williamszachary787@gmail.com>
2022-08-19 07:51:46 +0200

    update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
 
 const userRoute = require('./routes/userRoute.js')
 
-app.use("/users", userRoute)
+app.use("./users", userRoute)
 
 app.listen(app.get("port"), () => {
     console.log(`Listening for calls on port ${app.get("port")}`);
